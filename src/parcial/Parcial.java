16e04d9824501e02b3d03ad894d7cf20748c13de
james <jamesagomez2004@gmail.com>
2024-09-13 21:15:50 -0500

    Final de la division sin utilizar (/)

---
+++ src/parcial/Parcial.java
@@ -0,0 +1,30 @@
+package parcial;
+
+public class Parcial {
+    public static void main(String[] args) {
+  
+       int valorUno = 8;
+       int valorDos = 3;
+       int total = 0; 
+       int resultado = 0;
+       for(int i = valorDos; i <= valorUno; ){  
+        
+           total = (i += valorDos);
+           total = (total - valorDos);
+           resultado = ( resultado + 1);
+        }
+      
+      int resultante = (valorUno - total);
+        if(resultante == 0){
+            System.out.println("La division es exacta");
+        
+            }else{System.out.println("La divesion es inexacta");
+        }
+       
+        System.out.println("EL resultado de la division es: " + resultado);
+        System.out.println("el resultante es: " + resultante);
+    }
+        
+}
+    
+
