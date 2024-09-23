bf3a8235a2cdd4bf83aa00924ea6e3d2bad4111e
james <jamesagomez2004@gmail.com>
2024-09-23 04:37:39 -0500

    Se agrego Scanner, diferente manera el for

--- src/parcial/Parcial.java
+++ src/parcial/Parcial.java
@@ -1,28 +1,32 @@
 package parcial;
+import java.util.Scanner;
 
 public class Parcial {
     public static void main(String[] args) {
-  
-       int valorUno = 8;
-       int valorDos = 3;
-       int total = 0; 
-       int resultado = 0;
-       for(int i = valorDos; i <= valorUno; ){  
         
-           total = (i += valorDos);
-           total = (total - valorDos);
+       Scanner in = new Scanner(System.in);
+       
+       System.out.println("Digite el dividiendo");
+       int valorUno = in.nextInt();
+       
+       System.out.println("Digite el divisor");
+       int valorDos = in.nextInt();
+       int resultado = 0;
+       int i;
+       
+       for(i = valorDos; i <= valorUno;i=i+valorDos ){  
            resultado = ( resultado + 1);
         }
-      
-      int resultante = (valorUno - total);
-        if(resultante == 0){
+        i = (i-valorDos);
+      int residuo = (valorUno - i);
+        if(residuo == 0){
             System.out.println("La division es exacta");
         
             }else{System.out.println("La division es inexacta");
         }
        
         System.out.println("EL resultado de la division es: " + resultado);
-        System.out.println("El resultante es: " + resultante);
+        System.out.println("El residuo es: " + residuo);
     }
         
 }
